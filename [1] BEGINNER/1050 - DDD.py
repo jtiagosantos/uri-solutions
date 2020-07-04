75d183fd794cbf7235c2080ae006c18f25abcd5f
tiago040 <tiagosan040@gmail.com>
2020-07-04 09:25:32 -0400

    Solução para o problema 1050 - DDD

---
+++ [1] BEGINNER/1050 - DDD.py
@@ -0,0 +1,42 @@
+'''
+Leia um número inteiro que representa um código de DDD para discagem interurbana. Em seguida, informe à qual cidade o DDD pertence, considerando a tabela abaixo:
+
+DDD     DESTINATION
+61		Brasilia
+71		Salvador
+11		Sao Paulo
+21		Rio de Janeiro
+32		Juiz de Fora
+19		Campinas
+27		Vitoria
+31		Belo Horizonte
+
+Se a entrada for qualquer outro DDD que não esteja presente na tabela acima, o programa deverá informar:
+DDD nao cadastrado
+
+Entrada
+A entrada consiste de um único valor inteiro.
+
+Saída
+Imprima o nome da cidade correspondente ao DDD existente na entrada. Imprima DDD nao cadastrado caso não existir DDD correspondente ao número digitado.
+'''
+
+ddd = int(input())
+if ddd == 61:
+    print('Brasilia')
+elif ddd == 71:
+    print('Salvador')
+elif ddd == 11:
+    print('Sao Paulo')
+elif ddd == 21:
+    print('Rio de Janeiro')
+elif ddd == 32:
+    print('Juiz de Fora')
+elif ddd == 19:
+    print('Campinas')
+elif ddd == 27:
+    print('Vitoria')
+elif ddd == 31:
+    print('Belo Horizonte')
+else:
+    print('DDD nao cadastrado')
