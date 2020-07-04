3c650cfac0d8c0cc654045ef65ab2efd78def6e4
tiago040 <tiagosan040@gmail.com>
2020-07-04 09:00:01 -0400

    Solução para o problema 1044 - Múltiplos

---
+++ [1] BEGINNER/1044 - Múltiplos.py
@@ -0,0 +1,18 @@
+'''
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+Entrada
+A entrada contém valores inteiros.
+
+Saída
+A saída deve conter uma das mensagens conforme descrito acima.
+'''
+
+valores = str(input()).split()
+A = int(valores[0])
+B = int(valores[1])
+if A % B == 0 or B % A == 0:
+    print('Sao Multiplos')
+else:
+    print('Nao sao Multiplos')
+    
