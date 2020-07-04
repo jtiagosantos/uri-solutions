3e2f39da750f432058fd2bbb5913b4b07ee95c86
tiago040 <tiagosan040@gmail.com>
2020-07-04 09:37:43 -0400

    Solução para o problema 1059 - Pares

---
+++ [1] BEGINNER/1059 - Pares.py
@@ -0,0 +1,12 @@
+'''
+Faça um programa que mostre os números pares entre 1 e 100, inclusive.
+
+Entrada
+Neste problema extremamente simples de repetição não há entrada.
+
+Saída
+Imprima todos os números pares entre 1 e 100, inclusive se for o caso, um em cada linha.
+'''
+
+for i in range(2,101,2):
+    print(i)
