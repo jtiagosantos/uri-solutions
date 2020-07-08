72ac6e666438d843807a387ca4aa5810608bd0cf
tiago040 <tiagosan040@gmail.com>
2020-07-07 23:05:40 -0400

    Solução para o problema 1959 - Polígonos Regulares Simples

---
+++ [1] BEGINNER/1959 - Polígonos Regulares Simples.py
@@ -0,0 +1,18 @@
+'''
+Na geometria Euclidiana, um polígono regular é um polígono em que todos os ângulos são iguais e todos os lados tem o mesmo comprimento. Um polígono simples é aquele cujos segmentos de reta não se interceptam. Abaixo pode-se ver vários mosaicos feitos por polígonos regulares.
+
+https://www.urionlinejudge.com.br/gallery/images/contests/C_1.jpg
+
+Você deve escrever um programa que, dados o número e o comprimento dos lados de um polígono regular, mostre seu perímetro.
+
+Entrada
+A entrada tem dois inteiros positivos: N e L, que são, respectivamente, o número de lados e o comprimento de cada lado de um polígono regular (3 ≤ N ≤ 1000000 and 1 ≤ L ≤ 4000).
+
+Saída
+A saída é o perímetro P do polígono regular em uma única linha.
+'''
+
+entrada = str(input()).split()
+for k,v in enumerate(entrada):
+    entrada[k] = int(v)
+print(entrada[0] * entrada[1])
