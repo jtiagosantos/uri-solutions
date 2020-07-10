95f16fdb44af28dfedca78bdb0635f5195153297
tiago040 <tiagosan040@gmail.com>
2020-07-09 23:11:15 -0400

    Solução para o problema 2581 - I am Toorg!

---
+++ [1] BEGINNER/2581 - I am Toorg!.py
@@ -0,0 +1,15 @@
+'''
+Toorg é o integrante mais sábio do grupo de heróis denominado Os Protetores da Via Láctea. Para qualquer pergunta, ele tem a resposta ideal! Escreva um programa que, dada uma pergunta, informe a resposta de Toorg.
+
+Entrada
+A entrada terá diversos casos de teste. A cada caso de teste, um número N é apresentado, que representa o número de casos de teste. Em seguida, haverá N linhas, com as perguntas feitas para Toorg.
+
+Saída
+Para cada caso de teste, imprima a resposta de Toorg.
+'''
+
+N = int(input())
+for i in range(N):
+    perg = str(input())
+    print('I am Toorg!')
+    
