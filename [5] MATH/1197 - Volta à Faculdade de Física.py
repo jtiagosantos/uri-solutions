9d01d29916abc24e5d72f35fba90c5826d561919
tiago040 <tiagosan040@gmail.com>
2020-07-10 21:23:41 -0400

    Solução para o problema 1197 - Volta à Faculdade de Física

---
+++ [5] MATH/1197 - Volta à Faculdade de Física.py
@@ -0,0 +1,19 @@
+'''
+Uma partícula tem velocidade inicial e aceleração constante. Se a sua velocidade após certo momento é v então qual será seu deslocamento no dobro deste tempo?
+
+Entrada
+A entrada contém vários casos de teste. Cada linha da entrada é um caso de teste e contém dois inteiros v (-100 ≤ v ≤ 100) e t (0 ≤ t ≤ 200) ( t significa o momento no qual a partícula ganha aquela velocidade). O final da entrada é determinado por EOF.
+
+Saída
+Para cada linha de entrada imprima um único inteiro que denota o deslocamento no dobro do tempo fornecido.
+'''
+
+while True:
+    try:
+        entrada = str(input()).split()
+        v = int(entrada[0])
+        t = int(entrada[1])
+        d = v * t * 2
+        print(d)
+    except EOFError:
+        break
