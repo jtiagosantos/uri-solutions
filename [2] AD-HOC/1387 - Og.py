815dd321582a12b0b8154d8ca073549cf3246812
tiago040 <tiagosan040@gmail.com>
2020-07-16 14:37:23 -0400

    Solução para o problema 1387 - Og

---
+++ [2] AD-HOC/1387 - Og.py
@@ -0,0 +1,23 @@
+'''
+Og é um homem das cavernas com vários filhos e filhas, e ele quer contar todos eles. Og conta seus filhos com sua mão esquerda e suas filhas com sua mão direita.
+
+Entretanto, Og não é inteligente, e não sabe somar os dois números. Assim, ele pediu para você escrever um programa que realize a soma.
+
+Entrada
+A entrada contém vários casos de teste. Cada caso de teste consiste em uma linha contendo dois inteiros L e R, separados por um espaço, indicando o número de filhos e de filhas, respectivamente (1 ≤ L, R ≤ 5).
+
+O fim do arquivo de entrada é indicado por L = R = 0.
+
+Saída
+Para cada caso de teste, imprima uma linha contendo um inteiro indicando quantos filhos e filhas Og tem ao todo.
+'''
+
+while True:
+    entrada = str(input())
+    if entrada == '0 0':
+        break
+    entrada = entrada.split()
+    for k, v in enumerate(entrada):
+        entrada[k] = int(v)
+    print(sum(entrada))
+    
