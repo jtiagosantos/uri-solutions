74187ad4883e619545317fedc31c7fca2acf45d7
tiago040 <tiagosan040@gmail.com>
2020-07-18 23:39:59 -0400

    Solução para o problema 1257 - Array Hash

---
+++ [3] STRINGS/1257 - Array Hash.py
@@ -0,0 +1,45 @@
+'''
+Você terá como uma entrada várias linhas, cada uma com uma string. O valor de cada caracter é computado como segue:
+
+Valor = (Posição no alfabeto) + (Elemento de entrada) + (Posição do elemento)
+
+Todas posições são baseadas em zero. 'A' tem posição 0 no alfabeto, 'B' tem posição 1 no alfabeto, ... O cálculo de hash retornado é a soma de todos os caracteres da entrada. Por exemplo, se a entrada for:
+CBA
+DDD
+
+então cada caractere deverá ser computado como segue:
+
+2 = 2 + 0 + 0 : 'C' no elemento 0 posição 0
+2 = 1 + 0 + 1 : 'B' no elemento 0 posição 1
+2 = 0 + 0 + 2 : 'A' no elemento 0 posição 2
+4 = 3 + 1 + 0 : 'D' no elemento 1 posição 0
+5 = 3 + 1 + 1 : 'D' no elemento 1 posição 1
+6 = 3 + 1 + 2 : 'D' no elemento 1 posição 2
+
+O cálculo final de hash será 2+2+2+4+5+6 = 21.
+
+Entrada
+A entrada contém vários casos de teste. A primeira linha de entrada contém um inteiro N que indica a quantidade de casos de teste. Cada caso de teste inicia com um inteiro L (1 ≤ L ≤ 100) que indica a quantidade de linhas que vem a seguir. Cada uma destas L linhas contém uma string com até 50 letras maiúsculas ('A' - 'Z').
+
+Saída
+Para cada caso de teste imprima o valor de hash que é calculado conforme o exemplo apresentado acima.
+'''
+
+alfabeto = ['A','B','C','D','E','F','G','H','I','J','K','L',
+            'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+]
+
+N = int(input())
+for n in range(N):
+    valor = count = index = 0
+    resultado = []    
+    linhas = int(input())
+    for l in range(linhas):
+        string = str(input()).upper()
+        for s in string:
+            resultado.append(alfabeto.index(s) + count + index)
+            index += 1
+        count += 1
+        index = 0
+    print(sum(resultado))
+    
