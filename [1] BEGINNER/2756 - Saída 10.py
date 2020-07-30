e0fbb9d1932e5dc87df65d641d0c970178461b2a
tiago040 <tiagosan040@gmail.com>
2020-07-30 12:46:28 -0400

    Solução para o problema 2756 - Saída 10

---
+++ [1] BEGINNER/2756 - Saída 10.py
@@ -0,0 +1,28 @@
+'''
+O seu professor de programação gostaria que você fizesse um programa com as seguintes características:
+
+Coloque sete espaços em branco e coloque o carácter 'A';
+Coloque seis espaços em branco e coloque o carácter 'B', um espaço em branco e o carácter 'B';
+Coloque cinco espaços em branco e coloque o carácter 'C', três espaço em branco e o carácter 'C';
+Coloque quatro espaços em branco e coloque o carácter 'D', cinco espaço em branco e o carácter 'D';
+Coloque três espaços em branco e coloque o carácter 'E', sete espaço em branco e o carácter 'E';
+Repita o procedimento 4;
+Repita o procedimento 3;
+Repita o procedimento 2;
+Repita o procedimento 1.
+Entrada
+Não há.
+
+Saída
+O resultado de seu programa deve ser escrito conforme o exemplo de saída.
+'''
+
+print('A'.rjust(8))
+print('B'.rjust(7)+'B'.rjust(2))
+print('C'.rjust(6)+'C'.rjust(4))
+print('D'.rjust(5)+'D'.rjust(6))
+print('E'.rjust(4)+'E'.rjust(8))
+print('D'.rjust(5)+'D'.rjust(6))
+print('C'.rjust(6)+'C'.rjust(4))
+print('B'.rjust(7)+'B'.rjust(2))
+print('A'.rjust(8))
