5ffbee888f12c3f15f86ceed5d90e27f8ceb7122
tiago040 <tiagosan040@gmail.com>
2020-08-20 13:58:00 -0300

    Solução para o problema 2864 - Qual é a altura?

--- [5] MATH/2864 - Qual é a altura.py
+++ [5] MATH/2864 - Qual é a altura.py
@@ -4,7 +4,7 @@ Nick é um cientista que viaja por diversos universos paralelos, juntamente com 
 Entrada
 A entrada é composta por vários casos de teste. A primeira linha contém um número inteiro T (2 <= T <= 99) relativo ao número de casos de teste. As T linhas seguintes possuem três valores inteiros A (A < 0), B e C (-100 <= B, C <= 100), representando os coeficientes de uma função de segundo grau, na forma ax2 + bx + c.
 
-Saída
+Saída.
 Para cada caso de teste de entrada do seu programa, você deve imprimir um número real, com aproximação de duas casas decimais, a altura máxima do arremesso de uma fruta.
 '''
 
