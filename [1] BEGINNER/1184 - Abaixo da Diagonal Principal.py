0d41412a65abd96b45a92d60072ce3f09eb24945
tiago040 <tiagosan040@gmail.com>
2020-08-27 18:40:29 -0300

    Solução para o problema 1184 - Abaixo da Diagonal Principal

---
+++ [1] BEGINNER/1184 - Abaixo da Diagonal Principal.py
@@ -0,0 +1,35 @@
+'''
+Leia um caractere maiúsculo, que indica uma operação que deve ser realizada e uma matriz M[12][12]. 
+Em seguida, calcule e mostre a soma ou a média considerando somente aqueles elementos que estão abaixo 
+da diagonal principal da matriz, conforme ilustrado abaixo (área verde).
+
+https://resources.urionlinejudge.com.br/gallery/images/problems/UOJ_1184.png
+
+Entrada
+A primeira linha de entrada contem um único caractere Maiúsculo O ('S' ou 'M'), indicando a operação
+(Soma ou Média) que deverá ser realizada com os elementos da matriz. Seguem os 144 valores de ponto 
+flutuante que compõem a matriz.
+
+Saída
+Imprima o resultado solicitado (a soma ou média), com 1 casa após o ponto decimal.
+'''
+
+operacao = input().upper()
+
+matriz = []; elementos = []; i = 0
+
+for _ in range(12):
+    linha = []
+    for _ in range(12):
+        linha.append(float(input()))
+    matriz.append(linha)
+
+for linha in matriz:
+    elementos.append(sum(linha[:i]))
+    i += 1
+    
+if operacao == 'S':
+    print('{:.1f}'.format(sum(elementos)))
+if operacao == 'M':
+    print('{:.1f}'.format(sum(elementos)/66))
+    
