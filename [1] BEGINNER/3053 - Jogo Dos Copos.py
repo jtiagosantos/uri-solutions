4941fa7cb4e90192097a34be53039614f41c0fca
tiago040 <tiagosan040@gmail.com>
2020-09-03 10:32:13 -0300

    Solução para o problema 3053 - Jogo Dos Copos

---
+++ [1] BEGINNER/3053 - Jogo Dos Copos.py
@@ -0,0 +1,52 @@
+'''
+Uma brincadeira muito comum e divertida entre dois jogadores usa uma moeda e três copos opacos (ou seja, não é possível ver 
+o que está dentro do copo olhando pela lateral do copo). Os três copos são colocados com a boca para baixo, em uma linha, 
+um ao lado do outro, em posições que vamos chamar de A, B e C. Uma moeda é colocada embaixo de um dos copos. Na brincadeira, 
+um jogador chamado banca realiza um movimento para trocar a posição de dois copos, arrastando os copos de tal modo que se a 
+moeda está em baixo de um dos copos envolvidos no movimento, ela continua embaixo do mesmo copo após a troca de posição. 
+O jogador banca pode realizar três tipos de movimento, ilustrados na figura abaixo:
+
+1. Trocar o copo na posição A com o copo na posição B.
+
+2. Trocar o copo na posição B com o copo na posição C.
+
+3. Trocar o copo na posição A com o copo na posição C.
+
+O jogador banca realiza vários movimentos de troca tentando confundir o outro jogador, chamado espectador. Ao final o jogador 
+espectador deve dizer em qual posição está a moeda. Por exemplo, considere que inicialmente a moeda está embaixo do copo na 
+posição A e que o jogador banca realiza uma sequência de apenas três trocas, executando um movimento do tipo 1, após o qual 
+moeda termina embaixo do copo na posição B, seguido de um movimento do tipo 2, após o qual a moeda termina embaixo do copo 
+na posição C, seguido de um movimento do tipo 3, após o qual a moeda termina embaixo do copo na posição A.
+
+Nesta tarefa, dadas a descrição da sequência de movimentos e a posição inicial da moeda, você deve escrever um programa que 
+determine a posição final da moeda após todos os movimentos.
+
+https://resources.urionlinejudge.com.br/gallery/images/problems/UOJ_3040.png
+
+Entrada
+A primeira linha contém um inteiro N, o número de movimentos que o jogador banca realiza. A segunda linha contém um caractere, 
+entre A, B e C, indicando a posição inicial da moeda. Cada uma das N linhas seguintes contém um inteiro, indicando o tipo de 
+movimento efetuado pelo jogador banca na sequência.
+
+Saída
+Seu programa deve produzir uma única linha, com um único caractere entre A, B e C, a posição em que a moeda se encontra ao final da sequência de movimentos.
+
+Restrições 
+• 1 ≤ N ≤ 1000
+'''
+
+num_movimentos = int(input())
+letra = input().upper()
+copos = ['A', 'B', 'C']
+coposcopy = copos.copy()
+for _ in range(num_movimentos):
+    mov = int(input())
+    if mov == 1:
+        copos[0], copos[1] = copos[1], copos[0]
+    if mov == 2:
+        copos[1], copos[2] = copos[2], copos[1]
+    if mov == 3:
+        copos[0], copos[2] =  copos[2], copos[0]
+
+pos_final = copos.index(letra)
+print(coposcopy[pos_final])
