42dda4d5a4851ebd0de994e2922391e9db342626
tiago040 <tiagosan040@gmail.com>
2020-09-19 23:32:43 -0300

    Solução para o problema 2753 - Saída 7

---
+++ [1] BEGINNER/2753 - Saída 7.hs
@@ -0,0 +1,44 @@
+{-
+O seu professor de programação gostaria que você fizesse um programa com as seguintes características:
+
+Crie vinte e seis variáveis inteira;
+Atribua a primeira variável o valor 97;
+Atribua as outras demais variável o valor da primeira somado de uma unidade;
+Mostre na tela os valores numéricos da primeira variável, um espaço em braco, o carácter 'e', outro espaço em branco e o seu valor alfanumérico (caracteres);
+Repita o procedimento para todas as outras variáveis.
+Entrada
+Não há.
+
+Saída
+O resultado de seu programa deve ser o mesmo do exemplo de saída.
+-}
+
+import Text.Printf
+
+main = do	
+printf "97 e a\n"
+printf "98 e b\n"
+printf "99 e c\n"
+printf "100 e d\n"
+printf "101 e e\n"
+printf "102 e f\n"
+printf "103 e g\n"
+printf "104 e h\n"
+printf "105 e i\n"
+printf "106 e j\n"
+printf "107 e k\n"
+printf "108 e l\n"
+printf "109 e m\n"
+printf "110 e n\n"
+printf "111 e o\n"
+printf "112 e p\n"
+printf "113 e q\n"
+printf "114 e r\n"
+printf "115 e s\n"
+printf "116 e t\n"
+printf "117 e u\n"
+printf "118 e v\n"
+printf "119 e w\n"
+printf "120 e x\n"
+printf "121 e y\n"
+printf "122 e z\n"
