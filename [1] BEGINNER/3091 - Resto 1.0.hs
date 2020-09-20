1c9324a7d04d2c49cfa21a5a4c2a2967ae472807
tiago040 <tiagosan040@gmail.com>
2020-09-20 00:01:09 -0300

    Solução para o problema 3091 - Resto 1.0

---
+++ [1] BEGINNER/3091 - Resto 1.0.hs
@@ -0,0 +1,17 @@
+{-
+William, John é um aluno vidrado por matemática, mas poucas pessoas sabem disso. Certo dia, um de seus colegas de classe achando que John não sabia conceitos básicos da matemática o desafiou a realizar um simples cálculo que era: Calcular o resto da divisão de A ÷ B
+Como John é muito tímido e vocês são melhores amigos, ele te informou qual era a resposta para o problema e pediu para que você passasse tal informação para esse colega que o desafiou. Você pode fazer isso ?
+Dado dois inteiros A e B, diga qual o resto da divisão de A ÷ B que John passou para você.
+
+Entrada
+A entrada contém dois inteiros A e B (1 <= A, B <= 100000).
+
+Saída
+A saída contém um único inteiro que é o resto da divisão de A ÷ B.
+-}
+
+main = do
+a <- readLn :: IO Int
+b <- readLn :: IO Int
+let resto = rem a b
+putStrLn (show(resto))
