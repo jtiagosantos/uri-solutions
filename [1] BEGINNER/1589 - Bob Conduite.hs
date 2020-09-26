8c0a3672c649d7e16b0d2be306419b3db582033a
tiago040 <tiagosan040@gmail.com>
2020-09-26 20:18:16 -0300

    Solução para o problema 1589 - Bob Conduite

--- [1] BEGINNER/1589 - Bob Conduite.hs
+++ [1] BEGINNER/1589 - Bob Conduite.hs
@@ -1,4 +1,4 @@
-'''
+{-
 Você tem em mãos dois cabos circulares de energia. O primeiro cabo tem raio R1 e o segundo raio R2. Você precisa comprar um conduite circular (veja a imagem abaixo que ilustra um conduite) de maneira a passar os dois cabos por dentro dele:
 
 https://resources.urionlinejudge.com.br/gallery/images/problems/UOJ_1589.jpg
@@ -11,11 +11,18 @@ Na primeira linha teremos um inteiro T (T ≤ 10000), indicando o número de cas
 Na única linha de cada caso teremos dois números inteiros R1 e R2, indicando os respectivos raios. Os inteiros serão positivos e todas as contas caberão em um inteiro normal de 32 bits.
 Saída
 Em cada caso, imprima o menor raio possível em uma única linha
-'''
+-}
 
-T = int(input())
-for i in range(T):
-     valores = str(input()).split()
-     r1 = int(valores[0])
-     r2 = int(valores[1])
-     print('{}'.format(r1 + r2))
+loop 0 = return ()
+loop n = do
+  valores <- getLine
+  let lista = words valores
+  let a = lista !! 0
+  let b = lista !! 1
+  let soma = (read(a)) + (read(b))
+  print(soma)
+  loop (n-1)
+
+main = do
+  n <- readLn :: IO Int
+  loop n
