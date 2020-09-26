7b1c4f757058f825f6dac2f1b436e312f55da74b
tiago040 <tiagosan040@gmail.com>
2020-09-26 19:15:33 -0300

    Solução para o problema 1805 - Soma Natural

---
+++ [5] MATH/1805 - Soma Natural.py
@@ -0,0 +1,17 @@
+'''
+Um número natural é um inteiro não-negativo (0, 1, 2, 3, 4, 5,...). A sua tarefa neste problema é calcular a soma dos números 
+naturais que estão presentes em um determinado intervalo [A, B] inclusive.
+Por exemplo, a soma dos números naturais no intervalo [2, 5] é 14 = (2+3+4+5).
+
+Entrada
+Cada caso de teste contém dois inteiros A e B (1 ≤ A ≤ B ≤ 10^9), representando o limite inferior e o superior respectivamente.
+
+Saída
+Para cada caso de teste, a saída consiste de uma linha contendo a soma dos números naturais do intervalo.
+'''
+
+entrada = list(map(int, input().split()))
+a1 = entrada[0]
+an = entrada[1]
+soma = ((a1+an) * ((an-a1)+1))/2
+print(int(soma))
