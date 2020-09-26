a2d1f115f21285eccb195178647f3af6cae58627
tiago040 <tiagosan040@gmail.com>
2020-09-26 20:15:36 -0300

    Solução para o problema 1865 - Mjölnir

--- [1] BEGINNER/1865 - Mjölnir.hs
+++ [1] BEGINNER/1865 - Mjölnir.hs
@@ -1,4 +1,4 @@
-'''
+{-
 Odin criou para Thor a mais fiel e poderosa arma possível, o martelo Mjölnir. Feito de um minério místico especial chamado Uru e forjado no coração de uma estrela pelos Deuses ferreiros de Asgard, Brokk e Eitri, os lendários ferreiros.
 Um dia, Thor desafiou seus amigos para ver quem conseguia levantar o Mjölnir.
 Escreva um programa que, dado um nome, e a força, em Newtons, aplicado ao tentar levantar o Mjölnir, informar se a pessoa conseguiu ou não levantá-lo.
@@ -8,14 +8,21 @@ Um número inteiro C será informado, que será a quantidade de casos de teste. 
 
 Saída
 Para cada caso de teste imprima um caractere ‘Y’, caso a pessoa tenha conseguido levantar , ou ‘N’, caso não tenha conseguido.
-'''
+-}
 
-C = int(input())
-for i in range(C):
-    entrada = str(input()).split()
-    nome = entrada[0]
-    forca = int(entrada[1])
-    if nome == 'Thor' and forca >= 50:
-        print('Y')
-    else:
-        print('N')
+levantar :: String -> String
+levantar a | (a == "Thor") = "Y"
+           | otherwise = "N"
+
+loop 0 = return ()
+loop n = do
+  entrada <- getLine
+  let lista = words entrada
+  let nome = lista !! 0
+  let saida = levantar (nome)
+  putStrLn (saida)
+  loop (n-1)
+
+main = do
+  n <- readLn :: IO Int
+  loop n
