41a4102033e43668b023577d111d894d70b8f578
tiago040 <tiagosan040@gmail.com>
2020-09-26 22:09:16 -0300

    Solução para o problema 1000 - Hello World!

---
+++ [1] BEGINNER/1000 - Hello World!.hs
@@ -0,0 +1,16 @@
+{-
+https://resources.urionlinejudge.com.br/gallery/images/problems/UOJ_1000.png
+
+Bem-vindo ao URI Online Judge!
+
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+Entrada
+Este problema não possui nenhuma entrada.
+
+Saída
+Você deve imprimir a mensagem "Hello World!" conforme o exemplo abaixo.
+-}
+
+main = do
+  putStrLn "Hello World!"
