dc3d2a13988bd3bcbe74f4ec91c8abd195ddbefa
tiago040 <tiagosan040@gmail.com>
2020-09-26 21:02:47 -0300

    Solução para o problema 2839 - As Meias de Rangel

---
+++ [5] MATH/2839 - As Meias de Rangel.hs
@@ -0,0 +1,18 @@
+{-
+Por algum motivo desconhecido, Rangel só tem um par de meias de cada cor.
+Hoje ele está atrasado para ir a faculdade e ainda precisa pegar um par de meias, mas as meias estão todas bagunçadas.
+Dado o número de pares de meias na gaveta de Rangel, ele quer saber quantas meias ele precisa pegar, no mínimo, para ter pelo menos um par da mesma cor.
+
+Entrada
+Cada caso é composto de um único inteiro N (1 ≤ N ≤ 105) que corresponde a quantidade de pares de meias na gaveta.
+
+Saída
+Você deve imprimir uma linha com um único inteiro que corresponde a quantidade mínima de meias que Rangel precisa pegar.
+-}
+
+import Text.Printf
+
+main = do
+pares <- readLn :: IO Int
+let qt_meias = pares + 1
+printf "%d\n" qt_meias
