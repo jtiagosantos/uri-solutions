85dad72948ed5bde5ff4462382b1962c8ce85886
tiago040 <tiagosan040@gmail.com>
2020-10-01 20:58:48 -0300

    Solução para o problema 2896 - Aproveite a Oferta

---
+++ [1] BEGINNER/2896 - Aproveite a Oferta.hs
@@ -0,0 +1,30 @@
+{-
+Um supermercado está fazendo uma promoção de venda de refrigerantes. Se um dia você comprar refrigerantes e levar os cascos vazios no dia seguinte, ela troca cada conjunto de K garrafas vazias  por uma garrafa cheia. Um cliente quer aproveitar ao máximo essa oferta e por isso comprou várias garrafas no primeiro dia da promoção. Agora ele quer saber quantas garrafas terá ao final do segundo dia da promoção, se usá-la ao máximo.
+Faça um programa para calcular isso.
+
+Entrada
+A primeira linha de entrada contém inteiro T (1 ≤ T ≤ 10000) , que indica o número de casos de teste. Em cada uma das T linhas a seguir vêm dois inteiros N e K (1 ≤ K, N ≤ 10000),  respectivamente o número de refrigerantes comprados e o número de garrafas vazias para ganhar uma cheia.
+
+Saída
+Para cada caso de teste imprima o número de garrafas que o cliente terá no segundo dia, se aproveitar ao máximo a oferta.
+-}
+
+calculo_1 :: Int -> Int -> Int
+calculo_1 a b = div a b
+
+calculo_2 :: Int -> Int -> Int
+calculo_2 a b = mod a b
+
+loop 0 = return ()
+loop n = do
+  valores <- getLine
+  let lista = words valores
+  let a = lista !! 0
+  let b = lista !! 1
+  let saida = calculo_1 (read(a)) (read(b)) + calculo_2 (read(a)) (read(b))
+  print(saida)
+  loop(n-1)
+
+main = do
+  n <- readLn :: IO Int
+  loop(n)
