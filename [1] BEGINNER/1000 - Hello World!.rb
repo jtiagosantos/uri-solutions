19a6bf83e078b4671614d97defd850f311c0157c
tiago040 <tiagosan040@gmail.com>
2020-10-10 22:04:18 -0300

    Solução para o problema 1000 - Hello World!

---
+++ [1] BEGINNER/1000 - Hello World!.rb
@@ -0,0 +1,15 @@
+=begin
+https://resources.urionlinejudge.com.br/gallery/images/problems/UOJ_1000.png
+
+Bem-vindo ao URI Online Judge!
+
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+Entrada
+Este problema não possui nenhuma entrada.
+
+Saída
+Você deve imprimir a mensagem "Hello World!" conforme o exemplo abaixo.
+=end
+
+puts 'Hello World!'
