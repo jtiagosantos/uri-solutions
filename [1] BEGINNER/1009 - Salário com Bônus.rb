bfd8a0294935581c271efe68794d4dbde7a796f3
tiago040 <tiagosan040@gmail.com>
2020-10-10 23:39:03 -0300

    Solução para o problema 1009 - Salário com Bônus

---
+++ [1] BEGINNER/1009 - Salário com Bônus.rb
@@ -0,0 +1,16 @@
+=begin
+Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas, informar o total a receber no final do mês, com duas casas decimais.
+
+Entrada
+O arquivo de entrada contém um texto (primeiro nome do vendedor) e 2 valores de dupla precisão (double) com duas casas decimais, representando o salário fixo do vendedor e montante total das vendas efetuadas por este vendedor, respectivamente.
+
+Saída
+Imprima o total que o funcionário deverá receber, conforme exemplo fornecido.
+=end
+
+nome = gets.chomp
+salario = gets.chomp.to_f
+tt_vendas = gets.chomp.to_f
+comissao = tt_vendas * 0.15
+total = salario + comissao
+puts "TOTAL = R$ %.2f" % total
