8522fd290d6bc44d6d533d803e49e19caf81782d
tiago040 <tiagosan040@gmail.com>
2020-10-12 21:12:47 -0300

    Solução para o problema 1014 - Consumo

---
+++ [1] BEGINNER/1014 - Consumo.rb
@@ -0,0 +1,14 @@
+=begin
+Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros).
+
+Entrada
+O arquivo de entrada contém dois valores: um valor inteiro X representando a distância total percorrida (em Km), e um valor real Y representando o total de combustível gasto, com um dígito após o ponto decimal.
+
+Saída
+Apresente o valor que representa o consumo médio do automóvel com 3 casas após a vírgula, seguido da mensagem "km/l".
+=end
+
+distancia = gets.chomp.to_f
+tt_comb = gets.chomp.to_f
+consumo = distancia / tt_comb
+puts "%.3f km/l" % consumo
