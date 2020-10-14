475865b765de756082b7e7bf8194fba34b0a03d5
tiago040 <tiagosan040@gmail.com>
2020-10-14 16:13:59 -0300

    Solução para o problema 2755 - Saída 9

---
+++ [1] BEGINNER/2755 - Saída 9.rb
@@ -0,0 +1,19 @@
+=begin
+O seu professor de programação gostaria que você fizesse um programa com as seguintes características:
+
+Mostre a seguinte frase na tela: "Ro'b'er to\/" (Entre o r e o t tem uma tabulação);
+Mostre a seguinte frase na tela: (._.) ( l: ) ( .-. ) ( :l ) (._.);
+Mostre a seguinte frase na tela: (^_-)(-_-) (-_^);
+Mostre a seguinte frase na tela: ("_") ('.');
+
+Entrada
+Não há.
+
+Saída
+O resultado de seu programa deve ser escrito conforme o exemplo de saída.
+=end
+
+puts '"Ro\'b\'er	to\\/"'
+puts '(._.) ( l: ) ( .-. ) ( :l ) (._.)'
+puts '(^_-) (-_-) (-_^)'
+puts '("_") (\'.\')'
