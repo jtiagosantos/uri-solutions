f2e496373d190d9ebceb3c8e3ea732b47c4cfa1f
tiago040 <tiagosan040@gmail.com>
2020-10-23 14:00:14 -0300

    Solução para o problema 1042 - Sort Simples

---
+++ [1] BEGINNER/1042 - Sort Simples.rb
@@ -0,0 +1,14 @@
+=begin
+Leia 3 valores inteiros e ordene-os em ordem crescente. No final, mostre os valores em ordem crescente, uma linha em branco e em seguida, os valores na sequência como foram lidos.
+
+Entrada
+A entrada contem três números inteiros.
+
+Saída
+Imprima a saída conforme foi especificado.
+=end
+
+valores = gets.chomp.split.map{|valor| valor.to_i}
+puts valores.sort
+puts
+puts valores
