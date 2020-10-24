21a3bb5d253b9ec87b1cf3651e8fccdea3fb1833
tiago040 <tiagosan040@gmail.com>
2020-10-23 21:42:46 -0300

    Solução para o problema 1050 - DDD

---
+++ [1] BEGINNER/1050 - DDD.rb
@@ -0,0 +1,44 @@
+=begin
+Leia um número inteiro que representa um código de DDD para discagem interurbana. Em seguida, informe à qual cidade o DDD pertence, considerando a tabela abaixo:
+
+DDD     DESTINATION
+61		Brasilia
+71		Salvador
+11		Sao Paulo
+21		Rio de Janeiro
+32		Juiz de Fora
+19		Campinas
+27		Vitoria
+31		Belo Horizonte
+
+Se a entrada for qualquer outro DDD que não esteja presente na tabela acima, o programa deverá informar:
+DDD nao cadastrado
+
+Entrada
+A entrada consiste de um único valor inteiro.
+
+Saída
+Imprima o nome da cidade correspondente ao DDD existente na entrada. Imprima DDD nao cadastrado caso não existir DDD correspondente ao número digitado.
+=end
+
+ddd = gets.chomp.to_i
+case ddd
+    when 61
+        puts "Brasilia"
+    when 71
+        puts "Salvador"
+    when 11
+        puts "Sao Paulo"
+    when 21
+        puts "Rio de Janeiro"
+    when 32
+        puts "Juiz de Fora"
+    when 19
+        puts "Campinas"
+    when 27
+        puts "Vitoria"
+    when 31
+        puts "Belo Horizonte"
+    else
+        puts "DDD nao cadastrado"
+end
