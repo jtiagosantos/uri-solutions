21b62699837fdc640bf7fee680b838dbc51a536c
tiago040 <tiagosan040@gmail.com>
2020-10-23 22:08:51 -0300

    Solução para o problema 1060 - Números Positivos

---
+++ [1] BEGINNER/1060 - Números Positivos.rb
@@ -0,0 +1,19 @@
+=begin
+Faça um programa que leia 6 valores. Estes valores serão somente negativos ou positivos (desconsidere os valores nulos). A seguir, mostre a quantidade de valores positivos digitados.
+
+Entrada
+Seis valores, negativos e/ou positivos.
+
+Saída
+Imprima uma mensagem dizendo quantos valores positivos foram lidos.
+=end
+
+valores = []
+6.times do
+    n = gets.chomp.to_f
+    valores.push(n)
+end
+
+count = valores.select{|i| i > 0}
+
+puts "#{count.size} valores positivos"
