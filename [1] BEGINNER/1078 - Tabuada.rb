0e20fa7a047d7ebd0443603ab849b95a8af01962
tiago040 <tiagosan040@gmail.com>
2020-10-26 12:08:14 -0300

    Solução para o problema 1078 - Tabuada

---
+++ [1] BEGINNER/1078 - Tabuada.rb
@@ -0,0 +1,16 @@
+=begin
+Leia 1 valor inteiro N (2 < N < 1000). A seguir, mostre a tabuada de N:      
+1 x N = N      2 x N = 2N        ...       10 x N = 10N
+
+Entrada
+A entrada contém um valor inteiro N (2 < N < 1000).
+
+Saída
+Imprima a tabuada de N, conforme o exemplo fornecido.
+=end
+
+n = gets.chomp.to_i
+for i in (1..10)
+    mult = i * n
+    puts "#{i} x #{n} = #{mult}"
+end
