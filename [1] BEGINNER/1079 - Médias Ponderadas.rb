eb245b958fc33044f50348362e732188e5fbc8af
tiago040 <tiagosan040@gmail.com>
2020-10-26 13:19:56 -0300

    Solução para o problema 1079 - Médias Ponderadas

---
+++ [1] BEGINNER/1079 - Médias Ponderadas.rb
@@ -0,0 +1,32 @@
+=begin
+Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem peso 5.
+
+Entrada
+O arquivo de entrada contém um valor inteiro N na primeira linha. Cada N linha a seguir contém um caso de teste com três valores com uma casa decimal cada valor.
+
+Saída
+Para cada caso de teste, imprima a média ponderada dos 3 valores, conforme exemplo abaixo.
+=end
+
+class MediaPonderada
+    
+    def initialize v1,v2,v3
+        @v1 = v1
+        @v2 = v2
+        @v3 = v3
+    end
+
+    def media
+       return ((@v1*2) + (@v2*3) + (@v3*5)) / 10.0
+    end
+    
+end
+
+
+n = gets.chomp.to_i
+n.times do
+    notas = gets.chomp.split.map{|nota| nota.to_f}
+    inst = MediaPonderada.new notas[0],notas[1],notas[2]
+    saida = inst.media
+    puts "%.1f" % saida
+end
