6df88ecd90495358b251334a3ceb292f70c70fc3
tiago040 <tiagosan040@gmail.com>
2020-11-06 07:29:58 -0300

    Solução para o problema 1000 - Hello World!

---
+++ [1] BEGINNER/1000 - Hello World!.js
@@ -0,0 +1,18 @@
+/*
+https://resources.urionlinejudge.com.br/gallery/images/problems/UOJ_1000.png
+
+Bem-vindo ao URI Online Judge!
+
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+Entrada
+Este problema não possui nenhuma entrada.
+
+Saída
+Você deve imprimir a mensagem "Hello World!" conforme o exemplo abaixo.
+*/
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+console.log("Hello World!")
