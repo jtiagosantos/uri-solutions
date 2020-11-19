a62a103acae39e23d1ccdf1f9cbbc2491c223ea2
tiago040 <tiagosan040@gmail.com>
2020-11-19 20:11:52 -0300

    Solução para o problema 1000 - Hello World!

---
+++ [1] BEGINNER/1000 - Hello World!.c
@@ -0,0 +1,21 @@
+/*
+https://resources.urionlinejudge.com.br/gallery/images/problems/UOJ_1000.png
+
+Bem-vindo ao URI Online Judge!
+
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+Entrada
+Este problema não possui nenhuma entrada.
+
+Saída
+Você deve imprimir a mensagem "Hello World!" conforme o exemplo abaixo.
+*/
+
+#include <stdio.h>
+ 
+int main() {
+    printf("Hello World!\n");
+ 
+    return 0;
+}
