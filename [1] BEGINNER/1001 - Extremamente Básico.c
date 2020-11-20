ac21333a7f3b12143b61db1c2399fec8e7f9526e
tiago040 <tiagosan040@gmail.com>
2020-11-20 04:24:07 -0300

    Solução para o problema 1001 - Extremamente Básico

--- [1] BEGINNER/1001 - Extremamente Básico.c
+++ [1] BEGINNER/1001 - Extremamente Básico.c
@@ -1,14 +1,14 @@
 /*
-Leia 2 valores inteiros e armazene-os nas vari�veis A e B. Efetue a soma de A e B atribuindo o seu resultado na vari�vel X. 
-Imprima X conforme exemplo apresentado abaixo. N?o apresente mensagem alguma al�m daquilo que est� sendo especificado e n?o 
-esque�a de imprimir o fim de linha ap�s o resultado, caso contr�rio, voc� receber� "Presentation Error".
+Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. 
+Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não 
+esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
 
 Entrada
-A entrada cont�m 2 valores inteiros.
+A entrada contém 2 valores inteiros.
 
-Sa�da
-Imprima a mensagem "X = " (letra X mai�scula) seguido pelo valor da vari�vel X e pelo final de linha. Cuide para que tenha um 
-espa�o antes e depois do sinal de igualdade, conforme o exemplo abaixo.
+Saída
+Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha. Cuide para que tenha um 
+espaço antes e depois do sinal de igualdade, conforme o exemplo abaixo.
 */
 
 #include <stdio.h>
