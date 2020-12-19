d5fa14c6378c02fa8f77d6ec016cbfa3ece487a9
tiago040 <tiagosan040@gmail.com>
2020-12-19 20:01:00 -0300

    Solução para o problema Bolinhas de Natal

--- CONTEST DE NATAL 2020/Bolinhas de Natal.py
+++ CONTEST DE NATAL 2020/Bolinhas de Natal.py
@@ -1,8 +1,7 @@
 '''
-Amélia ama o Natal, e sua parte favorita nesta data é montar a árvore de natal! Ela adora decorar a árvore com bolinhas e luzes coloridas, para que ela fique brilhante e divertida! Porém, Amélia gosta das coisas bem distribuídas e exige que sua árvore não tenha mais que a metade de galhos em bolinhas. Assim, se sua árvore de natal tem G galhos, ela precisa G/2 bolinhas. Se o número G de galhos for ímpar, esse valor será arredondado para baixo.
-
+Amélia ama o Natal, e sua parte favorita nesta data é montar a árvore de natal! Ela adora decorar a árvore com bolinhas e luzes coloridas, para que ela fique brilhante e divertida! Porém, Amélia gosta das coisas bem distribuídas e exige que sua 
+árvore não tenha mais que a metade de galhos em bolinhas. Assim, se sua árvore de natal tem G galhos, ela precisa G/2 bolinhas. Se o número G de galhos for ímpar, esse valor será arredondado para baixo.
 Neste ano, Amélia resolveu atualizar sua árvore e irá comprar uma nova. Além disso, algumas de suas bolinhas quebraram, e ela vai precisar saber quantas novas bolinhas vai precisar comprar para manter sua árvore equilibrada do jeito que gosta!
-
 Para isso, ela quer sua ajuda! Dada a quantidade de bolinhas que Amélia tem e a quantidade de galhos que sua nova árvore terá, diga para Amélia quantas bolinhas de natal ela precisa comprar para decorar sua nova árvore!
 
 Entrada
